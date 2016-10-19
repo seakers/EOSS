c6d54312ac992bed186a981ccdd1ebe2d3b2be8f
nh295 <nh295@cornell.edu>
2016-10-19 17:21:43 -0400

    pulled and merged with Harris' new driving feature that uses fewer features in mrmr.

--- java/src/eoss/search/InnovizationSearch.java
+++ java/src/eoss/search/InnovizationSearch.java
@@ -23,6 +23,7 @@ import knowledge.operator.EOSSOperatorCreator;
 import mining.DrivingFeaturesGenerator;
 import mining.label.AbstractPopulationLabeler;
 import mining.label.LabelIO;
+import mining.label.NondominatedSortingLabeler;
 import org.moeaframework.Instrumenter;
 import org.moeaframework.algorithm.AbstractEvolutionaryAlgorithm;
 import org.moeaframework.analysis.collector.InstrumentedAlgorithm;
@@ -174,7 +175,6 @@ public class InnovizationSearch implements Callable<Algorithm> {
             }
 
             int nFuncEvals = instAlgorithm.getNumberOfEvaluations();
-dfg.getDrivingFeatures("AIAA_innovize_14767543531208280000_0_labels.csv", "AIAA_innovize_14767543531208280000_0_feat.txt", 2, nOpsToAdd);
             //Check if the operators need to be replaced
             if (ops.checkTrigger(alg)) {
                 System.out.println(String.format("Operator replacement event triggered at %d func eval", nFuncEvals));
